954dabc8211e2ce84cd5e604baa36b29543317ae
Ashour100 <interinstitute5@gmail.com>
2022-05-06 15:25:55 +0200

    Basic info added

---
+++ data/movies.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+class Movies{
+    public $name;
+    public $year;
+    public $director;
+    public $writer;
+    public $country;
+    public $language;
+    public $cast;
+
+
+    function __construct($name,$year,$director,$writer,$country,$language,$cast= null){
+        $this->name=$name;
+        $this->year=$year;
+        $this->director=$director;
+        $this->writer=$writer;
+        $this->country=$country;
+        $this->language=$language;
+        $this->cast=$cast;
+    }
+}
+?>
+
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include 'data/movies.php'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $TheDetails= new Movies('Red Notice','2021','Rawson Marshall Thurber','Rawson Marshall Thurber','Italy','English',['Dwayne Johnson','Ryan Reynolds','Gal Gadot','Ritu Arya']);
+        var_dump($TheDetails);
+    ?>
+</body>
+</html>
